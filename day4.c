90ef60480cd40b69868478c3a203025819d0c776
HitarthSingh <hitarth05052006@gmail.com>
2024-10-01 19:09:58 +0530

    Add files via upload

---
+++ day4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int X,A,B,C;
+printf("Enter The Values :");
+scanf("%d %d %d %d",&A,&B,&C,&X);
+if(A==X || B==X || C==X)
+{printf("Yes");} 
+else {printf("no");}
+return 0;
+}
+
