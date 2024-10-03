df45a2552f063dbe8459bf47eb0244d68c73d552
HitarthSingh <hitarth05052006@gmail.com>
2024-10-03 18:08:11 +0530

    Add files via upload

---
+++ day6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+int a,b;
+printf("Enter The Time Taken By A :");
+scanf("%d",&a);
+printf("Enter The Time Taken  By B :");
+scanf("%d",&b);
+printf("total points of a are %d\n", 500-(a*2));
+printf("total points of a are %d",1000-(b*4));
+
+return 0;
+}
