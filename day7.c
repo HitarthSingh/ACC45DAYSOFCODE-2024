c163e6f2e409801712ab4337c3c4f7ecbfad0fdb
HitarthSingh <hitarth05052006@gmail.com>
2024-10-04 18:57:39 +0530

    Add files via upload

---
+++ day7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    if(x+3<=10)
+	    {
+	        printf(" yes \n");
+	    }
+	    else
+	    {
+	        printf(" no \n");
+	    }
+	}
+	return 0;
+}
+
