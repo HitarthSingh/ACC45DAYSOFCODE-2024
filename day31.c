9d0252ac639b02c31faaac5703824f6026f91fd3
HitarthSingh <hitarth05052006@gmail.com>
2024-11-02 09:02:16 +0530

    Add files via upload

---
+++ day31.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(void) {
+	int T;
+scanf("%d",&T);
+while(T--)
+{
+    int a;
+    scanf("%d",&a);
+     printf("%d\n",a*(a-1));
+}
+	return 0;
+}
+
