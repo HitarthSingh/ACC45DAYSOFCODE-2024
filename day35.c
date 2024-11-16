5b14dd402ce2f96706da29ecd957c1544d431991
HitarthSingh <hitarth05052006@gmail.com>
2024-11-16 09:41:53 +0530

    Add files via upload

---
+++ day35.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,x,a,b;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d %d %d",&x,&a,&b);
+	    if(a*1+b*2>=x)
+	    printf("Qualify\n");
+	    else
+	    printf("NotQualify\n");
+	}
+	return 0;
+}
